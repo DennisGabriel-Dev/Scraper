11db8fd47cbbc52ee6d03eb47ffcf518280b4bbc
DennisGabriel <dennisgabr745@gmail.com>
2024-05-28 22:56:46 -0300

    Add threads into scraper

--- scraper.rb
+++ scraper.rb
@@ -1,29 +1,39 @@
 require "httparty"
 require "nokogiri"
 require "csv"
+require "parallel"
 
-response = HTTParty.get("https://www.scrapingcourse.com/ecommerce/")
-document = Nokogiri::HTML(response.body)
 Product = Struct.new(:url, :image, :name, :price)
+products = Array.new
 
-html_products = document.css("li.product")
+pages_to_scrape = [
+  "https://www.scrapingcourse.com/ecommerce/page/1/",
+	"https://www.scrapingcourse.com/ecommerce/page/3/",
+	"https://www.scrapingcourse.com/ecommerce/page/4/",
+	"https://www.scrapingcourse.com/ecommerce/page/5/", 
+	"https://www.scrapingcourse.com/ecommerce/page/6/"
+]
 
-products = []
+semaphore = Mutex.new
 
-html_products.each do | html_product |
-  url = html_product.css("a").first.attribute("href").value
-  image = html_product.css("img").first.attribute("src").value
-  name = html_product.css("h2").first.text
-  price = html_product.css("span").first.text
+Parallel.map(pages_to_scrape, in_threads: 4) do |page_to_scrape|
+  response = HTTParty.get(page_to_scrape)
+  document = Nokogiri.HTML5(response.body)
 
-  product = Product.new(url, image, name, price)
-
-  products.push(product)
+  html_products = document.css("li.product")
+  html_products.each do |html_product|
+    url = html_product.css("a").attribute("href").value
+    image = html_product.css("img").first.attribute("src").value
+    name = html_product.css("h2").first.text
+    price = html_product.css("span").first.text
+    semaphore.synchronize{
+      products << Product.new(url, image, name, price)
+    }
+  end
 end
-
 csv_headers = %w{url image name price}
-CSV.open("output.csv", "wb", write_headers: true, headers: csv_headers) do | csv |
-  products.each do | product |
+CSV.open("output.csv", "wb", write_headers: true, headers: csv_headers) do |csv|
+  products.each do |product|
     csv << product
   end
 end
